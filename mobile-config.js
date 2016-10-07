e58c44653fd7975e5d2a56257a8c1e6038289f5c
Vaibhav Lohani <vaibhav241196@gmail.com>
2016-10-07 20:17:12 +0530

    Remove ios platforms

--- mobile-config.js
+++ mobile-config.js
@@ -10,14 +10,14 @@ App.info({
     website: 'http://tasks.siteflu.com'
 });
 // Set up resources such as icons and launch screens.
-// App.icons({
-//     'iphone': 'icons/icon-60.png',
-//     'iphone_2x': 'icons/icon-60@2x.png',
-//     // ... more screen sizes and platforms ...
-// });
+App.icons({
+    // 'iphone': 'icons/icon-60.png',
+    // 'iphone_2x': 'icons/icon-60@2x.png',
+    // ... more screen sizes and platforms ...
+});
 App.launchScreens({
-    'iphone': 'splash/Default~iphone.png',
-    'iphone_2x': 'splash/Default@2x~iphone.png',
+    // 'iphone': 'splash/Default~iphone.png',
+    // 'iphone_2x': 'splash/Default@2x~iphone.png',
     // ... more screen sizes and platforms ...
 });
 // Set PhoneGap/Cordova preferences
